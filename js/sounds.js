10ef8712284aa547f02ac71e90acaead1b07ea10
Keira Weireter <111704565+KeiraWeireter@users.noreply.github.com>
2023-03-17 16:49:45 +0000

    Fri, Mar 17, 2023, 4:49 PM +00:00

--- js/sounds.js
+++ js/sounds.js
@@ -5,6 +5,7 @@ sounds.forEach(sound => {
     btn.classList.add('btn')
     btn.innerText = sound
     btn.addEventListener('click', () => {
-        document.getElementById(sound)
+        document.getElementById(sound).play
     })
+    document.getElementById('buttons').appendChild(btn)
 })
