29153d66cfdb9f6ed51c1b9b098c002c1f86de8c
Keira Weireter <111704565+KeiraWeireter@users.noreply.github.com>
2023-03-20 17:02:13 +0000

    Mon, Mar 20, 2023, 5:02 PM +00:00

--- js/jokes.js
+++ js/jokes.js
@@ -3,6 +3,11 @@ const jokeBtn = document.getElementById('jokeBtn')
 
 jokeBtn.addEventListener('click', generateJoke)
 
-function generateJoke() {
-    
+async function generateJoke() {
+    const res = await fetch('https://icanhazdadjoke.com', {
+        headers: {Accept: 'application/json',},
+    })
+
+    const data = await res.json()
+    jokeEl.innerHTML = data.joke
 }
